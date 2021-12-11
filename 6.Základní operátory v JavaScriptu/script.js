c7ea1f85a72fcd74bdc69c12b1175fb2cda1dd67
Taras Ishchuk <ishchuktaras@gmail.com>
2021-12-11 18:41:16 +0100

    commit basic Javascript operators

--- 6.Základní operátory v JavaScriptu/script.js
+++ 6.Základní operátory v JavaScriptu/script.js
@@ -1,5 +1,22 @@
-// alert("Ahoj, ja jsem Taras!");
-// confirm("Jsi plnoletý?");
-var age = prompt("Kolik je tí let?");
-var adult = age - 18;
-console.log(adult);
+/********************
+	Základní operatory
+*/ 
+
+var ageTaras = 40;
+var ageMarta = 30;
+
+var birthdayTaras = 2021 - 40;
+var birthdayMarta = 2021 - 30;
+
+console.log(birthdayTaras);
+console.log(birthdayMarta);
+
+console.log(birthdayMarta / 2);
+console.log(birthdayMarta * 2);
+console.log(birthdayMarta + 300);
+console.log(birthdayMarta - 500);
+console.log(birthdayMarta * 2 - 800);
+
+console.log(5 + 10 * 5 + 10);
+
+
