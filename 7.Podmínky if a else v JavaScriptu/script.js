50583c92255d46cdc0a64f96ceb67d9059ec0b5c
Taras Ishchuk <ishchuktaras@gmail.com>
2021-12-11 19:59:00 +0100

    commit new folder

---
+++ 7.Podmínky if a else v JavaScriptu/script.js
@@ -0,0 +1,31 @@
+/********************
+	Základní operatory
+*/ 
+
+var ageTaras = 40;
+var ageMarta = 30;
+
+var birthdayTaras = 2021 - 40;
+var birthdayMarta = 2021 - 30;
+
+console.log(birthdayTaras);
+console.log(birthdayMarta);
+
+console.log(birthdayMarta / 2);
+console.log(birthdayMarta * 2);
+console.log(birthdayMarta + 300);
+console.log(birthdayMarta - 500);
+console.log(birthdayMarta * 2 - 800);
+
+console.log(5 + 10 * 5 + 10);
+
+/********************
+	Logické operatory
+*/ 
+
+// <  >  <==  ==>  ==
+
+var result = ageTaras > ageMarta;
+console.log(result);
+
+
