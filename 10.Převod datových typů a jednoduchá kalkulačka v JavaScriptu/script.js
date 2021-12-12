25886314b75cab4788b59d131381440f3ef5501f
Taras Ishchuk <ishchuktaras@gmail.com>
2021-12-12 15:38:07 +0100

    commit variables and test typeof values

---
+++ 10.Převod datových typů a jednoduchá kalkulačka v JavaScriptu/script.js
@@ -0,0 +1,6 @@
+var firstNumber = prompt("Zadej první čislo:");
+var secondNamber = prompt("Zadej druhé čislo:");
+var sign = prompt("Zadej plus nebo minus");
+
+console.log(typeof(firstNumber));
+console.log(typeof(secondNamber));
