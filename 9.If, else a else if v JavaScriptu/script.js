93c6355c6900e5223bc1988506fb59f77e03ada9
Taras Ishchuk <ishchuktaras@gmail.com>
2021-12-12 15:19:49 +0100

    commit if, else if  conditions

---
+++ 9.If, else a else if v JavaScriptu/script.js
@@ -0,0 +1,10 @@
+var ageTaras = 30;
+var ageMarta = 30;
+
+if (ageTaras > ageMarta) {
+	console.log("Taras je starší než Marta.");
+} else if (ageTaras < ageMarta) {
+	console.log("Taras je mladší než Marta.");
+} else {
+	console.log("Taras i Marta jsou stejně staří.");
+}
