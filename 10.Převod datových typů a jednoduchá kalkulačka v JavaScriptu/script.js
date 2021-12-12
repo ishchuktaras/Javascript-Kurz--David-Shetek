43420c37b910dae2b3a322cf492fa9e782dbaca1
Taras Ishchuk <ishchuktaras@gmail.com>
2021-12-12 15:56:42 +0100

    commit simplified code

--- 10.Převod datových typů a jednoduchá kalkulačka v JavaScriptu/script.js
+++ 10.Převod datových typů a jednoduchá kalkulačka v JavaScriptu/script.js
@@ -1,12 +1,9 @@
-var firstNumber = prompt("Zadej první čislo:");
-var secondNamber = prompt("Zadej druhé čislo:");
+var firstNumber = Number(prompt("Zadej první čislo:"));
+var secondNamber = Number(prompt("Zadej druhé čislo:"));
 var sign = prompt("Zadej plus nebo minus");
 
 console.log(typeof(firstNumber));
 console.log(typeof(secondNamber));
-
-firstNumber = Number(firstNumber);
-secondNamber = Number(secondNamber);
 
 if (sign == "+") {
 	console.log(firstNumber + secondNamber);
