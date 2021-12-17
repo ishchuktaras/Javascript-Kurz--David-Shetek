56cd2755886e9531869e578064a1623c9ea317ff
Taras Ishchuk <ishchuktaras@gmail.com>
2021-12-17 14:33:11 +0200

    commit lesson's files

---
+++ 12.Kalkučka, aby nešlo dělit nulou v JavaScriptu/script.js
@@ -0,0 +1,13 @@
+var firstNumber = Number(prompt("Zadej první čislo:"));
+var secondNamber = Number(prompt("Zadej druhé čislo:"));
+var sign = prompt("Zadej matematickou operaci");
+
+console.log(typeof(firstNumber));
+console.log(typeof(secondNamber));
+
+if (sign == "+") {
+	console.log(firstNumber + secondNamber);
+} else if (sign == "-") {
+	console.log(firstNumber - secondNamber);
+} else {
+	console.log("Tutu matematickou operace nezmám.");
