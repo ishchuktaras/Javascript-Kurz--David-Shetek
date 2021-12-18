f2c2f2e59b41c53f2636339e785dad47b5604215
Taras Ishchuk <ishchuktaras@gmail.com>
2021-12-18 17:25:35 +0200

    first commit new lesson's files

--- 13.Ternární operátor v JavaScriptu/script.js
+++ 13.Ternární operátor v JavaScriptu/script.js
@@ -1,20 +1,3 @@
-var firstNumber = Number(prompt("Zadej první čislo:"));
-var secondNamber = Number(prompt("Zadej druhé čislo:"));
-var sign = prompt("Zadej znak pro matematickou operaci");
-
-if (sign == "+") {
-	console.log(firstNumber + secondNamber);
-} else if (sign == "-") {
-		console.log(firstNumber - secondNamber);
-}	else if (sign == "*") {
-		console.log(firstNumber * secondNamber);
-}	else if (sign == "/" && secondNamber != 0) {
-		console.log(firstNumber / secondNamber);
-} else if (sign == "/" && secondNamber == 0) {
-	console.log("Nesmíš dělít nulou. Druhé číslo nesmí být nula pří dělení");
-} else {
-	console.log("Nedokáží takovouto operací provést.");
-}
 
 
 
