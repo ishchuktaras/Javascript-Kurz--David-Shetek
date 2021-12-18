3c57d19d0b7b15e956200b2e384961c502b95e66
Taras Ishchuk <ishchuktaras@gmail.com>
2021-12-18 17:55:02 +0200

    commit conditional (ternary) operator

--- 13.Ternární operátor v JavaScriptu/script.js
+++ 13.Ternární operátor v JavaScriptu/script.js
@@ -1,9 +1,14 @@
+
 var ageTaras = 10;
 
-if (ageTaras >= 18) {
-	console.log("Taras je dospělý");
-} else {
-	console.log("Taras je mladiství");
-}
+//if (ageTaras >= 18) {
+//	console.log('Taras je dospělý');
+//} else {
+//	console.log('Taras je mladiství');
+//}
 
+/*********************
+Ternární operátor
+*/
 
+ageTaras >= 18 ? console.log('Taras je dospělý'): console.log('Taras je mladiství');
