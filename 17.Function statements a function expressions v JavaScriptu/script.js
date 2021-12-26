14514c496a2b01bb29c1f16cdc3df03793dbb6ad
Taras Ishchuk <ishchuktaras@gmail.com>
2021-12-26 14:39:55 +0200

    commit Function expressions one more example

--- 17.Function statements a function expressions v JavaScriptu/script.js
+++ 17.Function statements a function expressions v JavaScriptu/script.js
@@ -15,3 +15,19 @@ var calculateAge = function(bith){
 	return 2021 - bith;
 }
 console.log(calculateAge(1981));
+
+//Function expressions II
+
+var myJob = function (job, firstName) {
+	switch (job) {
+		case 'učitel':
+			return firstName + ' je ' + job;
+		case 'manager/ka':
+			return firstName + ' je ' + job; 	
+		default:
+			return 'Nevím co má ' + firstName + ' za práci.';
+	}
+}
+console.log(myJob('učitel','David'));
+console.log(myJob('manager/ka','Eva'));
+console.log(myJob('zednik','Taras'));
