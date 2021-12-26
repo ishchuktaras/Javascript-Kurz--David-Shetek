6a7d558d5e35118c9bf1558718ad4f62f395fbbf
Taras Ishchuk <ishchuktaras@gmail.com>
2021-12-26 14:22:15 +0200

    commit Function statements a function expressions examples

--- 17.Function statements a function expressions v JavaScriptu/script.js
+++ 17.Function statements a function expressions v JavaScriptu/script.js
@@ -2,3 +2,16 @@
 *  Funkction
 */
 
+// Function statements
+
+// function calculateAge(bith) {
+	// return 2021 - bith;	
+// }
+// console.log(calculateAge(2000));
+
+//Function expressions
+
+var calculateAge = function(bith){
+	return 2021 - bith;
+}
+console.log(calculateAge(1981));
