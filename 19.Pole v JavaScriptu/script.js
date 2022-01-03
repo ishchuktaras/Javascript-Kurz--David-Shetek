5262aac3a0a9083609122dd16d138e750172eeb5
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-03 12:36:10 +0200

    commit files 20th lesson

---
+++ 19.Pole v JavaScriptu/script.js
@@ -0,0 +1,9 @@
+/************
+***Array (Pole) 
+***/
+
+var employees = ["David" ,"Jane", "Diana"];
+console.log(employees);
+console.log(employees[0]);
+console.log(employees[1]);
+console.log(employees[2]);
