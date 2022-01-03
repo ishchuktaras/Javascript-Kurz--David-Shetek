6753a3abbfe431ce101acff6f480bf279dd28fd5
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-03 12:54:05 +0200

    commit hodnoty do pole

---
+++ 20.Práce s polem v JavaScriptu/script.js
@@ -0,0 +1,12 @@
+/************
+*    Array (Pole) 
+*/
+
+var employees = ['David' ,'Jane', 'Diana'];
+
+employees[0]='Martin';
+employees[1]='Anna';
+employees[2]='Eva';
+employees[5]='Eduard';
+
+console.log(employees);
