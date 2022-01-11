2030961ac80362fec5850a21c8586a6ac7b61161
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-11 10:27:05 +0100

    commit 22th lesson

---
+++ 22.Objekty v JavaScriptu/script.js
@@ -0,0 +1,11 @@
+/* **************
+* Objects (Objekty)
+*/
+
+// Pole = hodnotám jsou přiřazené indexů; záleží na pořadi
+
+var employees =['David', 'Jana', 'Diana'];
+
+// Objekt = hodnotám jsou přirazené jiné hodnoty; nezáleží na pořadi
+
+
