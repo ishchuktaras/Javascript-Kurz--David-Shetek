d3e2d95ce2135dba184402059210d1d804a7d6c7
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-11 10:53:15 +0100

    commit one more type objects

--- 22.Objekty v JavaScriptu/script.js
+++ 22.Objekty v JavaScriptu/script.js
@@ -20,3 +20,12 @@ console.log(taras);
 console.log(taras.secondName);
 console.log(taras['secondName']);
 
+// Druhá možnost, jak zapsat objekt
+
+var marta = new Object();
+marta.firstName = 'Marta';
+marta.secondName = 'Vavzyniak';
+marta.birthYear = '1991';
+marta.driveLicence = 'false';
+
+console.log(marta);
