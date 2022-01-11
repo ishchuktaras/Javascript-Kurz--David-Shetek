93ab875e3f63a0692da4fe34c94b3ec4fce03e3d
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-11 10:09:42 +0100

    commit indexOff

--- 21.Pole a indexOf a length v JavaScriptu/script.js
+++ 21.Pole a indexOf a length v JavaScriptu/script.js
@@ -2,4 +2,9 @@
 *    Array (Pole) 
 */
 
-var employees = ['David' ,'Jana', 'Diana'];
+var employees = ['David' ,'Jana', 'Diana', 'Martin', 'Eduard'];
+
+console.log(employees.indexOf('David'));
+console.log(employees.indexOf('Eduard'));
+console.log(employees.indexOf('Natalia'));
+console.log(employees.indexOf('Petr'));
