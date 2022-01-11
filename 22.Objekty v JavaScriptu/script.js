8b5478e3a6e35ecd0b99044078d86983d612ce05
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-11 10:42:13 +0100

    commit console.log variable

--- 22.Objekty v JavaScriptu/script.js
+++ 22.Objekty v JavaScriptu/script.js
@@ -8,4 +8,15 @@ var employees =['David', 'Jana', 'Diana'];
 
 // Objekt = hodnotám jsou přirazené jiné hodnoty; nezáleží na pořadi
 
+var taras = {
+	firstName: 'Taras',
+	secondName: 'Ishchuk',
+	birthYear: '1981',
+	driveLicence: true,
+	family: ['Matvii', 'Eva', 'Sofia', 'Angelina', 'Marta']
+}
+
+console.log(taras);
+console.log(taras.secondName);
+console.log(taras['secondName']);
 
