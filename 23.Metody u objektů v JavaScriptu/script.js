8f3a50233737db6b271de4d98f8160ae23f1dea5
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-15 13:12:50 +0100

    commit settings VS Code

---
+++ 23.Metody u objektů v JavaScriptu/script.js
@@ -0,0 +1,11 @@
+/* **************
+ * Objects (Objekty)
+ */
+
+var taras = {
+    firstName: 'Taras',
+    secondName: 'Ishchuk',
+    birthYear: '1981',
+    driveLicence: true,
+    family: ['Matvii', 'Eva', 'Sofia', 'Angelina', 'Marta']
+}
