11b8e297f34601005a5449ef1d06caf7e8565d03
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-15 18:18:35 +0100

    commit

--- 24.Cykly: cyklus for/script.js
+++ 24.Cykly: cyklus for/script.js
@@ -1,3 +1,15 @@
 /* **************
  * Cykly
  */
+// Vypis bez cyklu
+
+console.log(0);
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+
+// Vypis zcyklem
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
