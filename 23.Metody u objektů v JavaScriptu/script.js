ce05594547e24d7e341a520bf219a69dd6356f2e
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-15 13:34:35 +0100

    commit metod u objektu

--- 23.Metody u objektů v JavaScriptu/script.js
+++ 23.Metody u objektů v JavaScriptu/script.js
@@ -2,10 +2,15 @@
  * Objects (Objekty)
  */
 // Metody u objektu = funkce
+
 var taras = {
     firstName: 'Taras',
     secondName: 'Ishchuk',
     birthYear: '1981',
     driveLicence: true,
-    family: ['Matvii', 'Eva', 'Sofia', 'Angelina', 'Marta']
+    family: ['Matvii', 'Eva', 'Sofia', 'Angelina', 'Marta'],
+    calculateAge: function(birthYear) {
+        return 2022 - birthYear;
+    }
 }
+console.log(taras.calculateAge(1981));
