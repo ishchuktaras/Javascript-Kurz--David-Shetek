08e6a6a1628dc89a71ff01c8efd2c35af8c3cf18
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-26 16:01:14 +0100

    ukoly pro cvičeni

--- 26.Cykly: příklady na cyklus for/script.js
+++ 26.Cykly: příklady na cyklus for/script.js
@@ -1,3 +1,11 @@
 /* **************
  * Procházení pole cyklem for
  */
+
+// Vypíšte 12x své jméno a příjmeni
+
+// Vypíšte čísla od 0 do 10
+
+// Vypíšte čísla od 0 do 50 včetně, ale jen násobky pětí
+
+// Vypíšte čísla od 15 do 0 (tedy v obráceném pořadí)
