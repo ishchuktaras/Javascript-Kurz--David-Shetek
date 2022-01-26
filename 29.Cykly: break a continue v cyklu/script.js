ceafaa176c26b16c934c763de91c0d44f1cd60b7
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-26 18:48:27 +0100

    Složka Break a Continue v cyklu

---
+++ 29.Cykly: break a continue v cyklu/script.js
@@ -0,0 +1,10 @@
+/* **************
+ * Do while cyklus
+ */
+
+/*
+break - způsobí ukončení cyklus
+continue - zastaví průchod cyklem, ale cyklus pokračuje dál (např. zastaví první průchod
+cyklem, ale pokračuje druhým opakováním)
+
+*/
