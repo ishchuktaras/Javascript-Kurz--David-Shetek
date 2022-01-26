63f5480e58da3b31eae7c2d2dc2107d7a6ff481b
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-26 16:44:21 +0100

    Přiklád cyklusu while

--- 27.Cykly: While cyklus/script.js
+++ 27.Cykly: While cyklus/script.js
@@ -2,3 +2,30 @@
  * While cyklus
  */
 
+var i = 0;
+while(i < 10){
+	console.log(i);
+	i++;
+}
+
+// For cyklus - když známe přesný počet opakování
+
+// While - neznáme přesný počet opakování
+
+/********
+Kolikrát můžete dané číslo dělit 2 tak, aby výsledek byl větší nebo rovno 1.
+
+12 : 2 = 6
+6 : 2 = 3
+3 : 2 = 1,5
+1,5 : 2 = 0,75
+
+
+40 : 2 = 20
+20 : 2 = 10
+10 : 2 = 5
+5 : 2 = 2,5
+2,5 : 2 = 1,25
+1,25 : 2 = 0,625
+
+*/
