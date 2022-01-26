88f131499855fd2b6bc77df1ca19e095886c7145
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-26 18:45:22 +0100

    Přiklad Do while cyklusu a porovnáni

--- 28.Cykly: Do while cyklus a porovnání všech cyklů/script.js
+++ 28.Cykly: Do while cyklus a porovnání všech cyklů/script.js
@@ -2,3 +2,22 @@
  * Do while cyklus
  */
 
+// For - známe počet opakování
+for (var i = 0; i < 0; i++){
+	console.log('Taras Ishchuk');
+}
+
+// While - neznáme počet opakování, nemusí se provést ani jednou
+var i = 0;
+while (i < 0){
+	console.log('Taras Ishchuk');
+	i++;
+}
+
+// Do while - neznáme počet opakování, provede se minimálně jednou
+var i = 0;
+	do {
+		console.log('Taras Ishchuk');
+		i++;
+	} while (i < 0); 
+	
