959f0026227318603c2663a5c16369160a568853
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-26 16:23:20 +0100

    Vyplnění všechný ukoly

--- 26.Cykly: příklady na cyklus for/script.js
+++ 26.Cykly: příklady na cyklus for/script.js
@@ -3,9 +3,21 @@
  */
 
 // Vypíšte 12x své jméno a příjmeni
+for (var i = 0; i < 12; i++){
+	console.log('Taras Ishchuk');
+}
 
 // Vypíšte čísla od 0 do 10
+for (var i = 0; i <= 10; i++){
+	console.log(i);
+}
 
-// Vypíšte čísla od 0 do 50 včetně, ale jen násobky pětí
+// Vypíšte čísla od 5 do 50 včetně, ale jen násobky pětí
+for (var i = 5; i <= 50; i = i + 5){
+	console.log(i);
+}
 
 // Vypíšte čísla od 15 do 0 (tedy v obráceném pořadí)
+for (var i = 15; i >= 0; i--){
+	console.log(i);
+}
