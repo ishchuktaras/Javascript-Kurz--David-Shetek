fee12336000807c71333d5f3d965e618d2fea2e7
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-26 19:17:32 +0100

    Break a continue

--- 29.Cykly: break a continue v cyklu/script.js
+++ 29.Cykly: break a continue v cyklu/script.js
@@ -8,3 +8,14 @@ continue - zastaví průchod cyklem, ale cyklus pokračuje dál (např. zastaví
 cyklem, ale pokračuje druhým opakováním)
 
 */
+
+var wanted = ['Killer', 'Bloody', 'Hacker'];
+var treveler = prompt();
+ for (var i = 0; i < 3; i++){
+	 console.log(i);
+	if (wanted[i] === treveler){
+		console.log('Zadržte ho!');
+		continue;
+	}
+	console.log("Toto se neprovede poté, co se narazí na continue");
+ }
