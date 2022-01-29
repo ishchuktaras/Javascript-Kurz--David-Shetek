3f2ae4938dbbd8c6c9c8b790e08c70551149b4c8
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-29 13:54:45 +0100

    Nová složka lekce Další eventy

---
+++ 41.Další eventy/script.js
@@ -0,0 +1,8 @@
+// click - kliknutí
+// dblclick - dvojité kliknutí
+// wheel - kolečko myši
+// mousedown - kliknete na tlačítko myši směrem dolů
+// mouseenter - najedeme myší
+// mouseleave - odejetí myši
+// mousemove - jakékoli pohnutí myši
+// mouseover - myší nad elementem
