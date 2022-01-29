37f46dd3125ef9ee3fcebb8555cd730ddd6622c4
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-29 14:16:00 +0100

    Další eventy fungují

--- 41.Další eventy/script.js
+++ 41.Další eventy/script.js
@@ -6,3 +6,7 @@
 // mouseleave - odejetí myši
 // mousemove - jakékoli pohnutí myši
 // mouseover - myší nad elementem
+
+document.querySelector("h1").addEventListener("mouseover",function(){
+	console.log("Event byl spuštěn");
+});
