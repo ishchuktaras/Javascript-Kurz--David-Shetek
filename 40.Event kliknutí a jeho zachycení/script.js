b4b7ff916fe8db7dd919ce2e0ede77e389a9364a
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-29 13:48:20 +0100

    Napojení udalostí pomoci anonymné funkce

--- 40.Event kliknutí a jeho zachycení/script.js
+++ 40.Event kliknutí a jeho zachycení/script.js
@@ -1,9 +1,17 @@
 // Event - zpráva, která se posílá kódu, že se na webu něco stalo. Např. bylo na něco kliknuti, scrollovalo se, změněna velikost okna, kliknuto na určitou klávesu atd.
 // Event listener - funkce, která čeká na určitý event a pak něco udělá
 
-function welcome (){
-	console.log("Vítejte u nás");
-}
+// function welcome (){
+// 	console.log("Vítejte u nás");
+// }
 // welcome();
 
-document.querySelector("h1").addEventListener("click",welcome);
+// Callback function
+
+// document.querySelector("h1").addEventListener("click",welcome); 
+
+// Anonymní funkce
+
+document.querySelector("h1").addEventListener("click",function(){
+	console.log("Vítejte u nás");
+}); 
