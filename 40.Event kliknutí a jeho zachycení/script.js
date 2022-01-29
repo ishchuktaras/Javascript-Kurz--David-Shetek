eaf61766526cd62c11dc1c9af0cc9399815f8266
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-29 13:35:31 +0100

    Pridání funkce

--- 40.Event kliknutí a jeho zachycení/script.js
+++ 40.Event kliknutí a jeho zachycení/script.js
@@ -1,3 +1,7 @@
-// Event - zpráva, která se posílá kódu, že se na webu něco stalo. Např. bylo na něco
-//   kliknuti, scrollovalo se, změněna velikost okna, kliknuto na určitou klávesu atd.
+// Event - zpráva, která se posílá kódu, že se na webu něco stalo. Např. bylo na něco kliknuti, scrollovalo se, změněna velikost okna, kliknuto na určitou klávesu atd.
 // Event listener - funkce, která čeká na určitý event a pak něco udělá
+
+function welcome (){
+	console.log("Vítejte u nás");
+}
+welcome();
