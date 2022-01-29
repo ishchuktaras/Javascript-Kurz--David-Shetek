03a87d4f3d35688842f6bf9fcf1ca902258cff65
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-29 13:21:39 +0100

    Nová složka Event kliknutí a jeho zachycení

---
+++ 40.Event kliknutí a jeho zachycení/script.js
@@ -0,0 +1,3 @@
+// Event - zpráva, která se posílá kódu, že se na webu něco stalo. Např. bylo na něco
+//   kliknuti, scrollovalo se, změněna velikost okna, kliknuto na určitou klávesu atd.
+// Event listener - funkce, která čeká na určitý event a pak něco udělá
