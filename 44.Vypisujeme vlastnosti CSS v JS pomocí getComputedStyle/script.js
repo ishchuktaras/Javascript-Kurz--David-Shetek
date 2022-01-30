b5863ca7000a58a03574d4b6187020eed82e5f7d
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-30 19:19:07 +0100

    Zjednodušení kodu

--- 44.Vypisujeme vlastnosti CSS v JS pomocí getComputedStyle/script.js
+++ 44.Vypisujeme vlastnosti CSS v JS pomocí getComputedStyle/script.js
@@ -3,20 +3,20 @@ var cube = document.querySelector("#cube");
 var style = getComputedStyle(cube);
 
 document.body.addEventListener("keydown", function(event){
+
+	var oldleft = parseInt(style.left);
+	var oldtop = parseInt(style.top);
+
 	if(event.key === "ArrowRight"){
-		var oldleft = parseInt(style.left);
 		var newleft = oldleft + 5;
 		cube.style.left = newleft + "px";
 	} else if(event.key === "ArrowLeft"){
-		var oldleft = parseInt(style.left);
 		var newleft = oldleft - 5;
 		cube.style.left = newleft + "px";
 	} else if(event.key === "ArrowUp"){
-		var oldtop = parseInt(style.top);
 		var newtop = oldtop - 5;
 		cube.style.top = newtop + "px";
 	} else if(event.key === "ArrowDown"){
-		var oldtop = parseInt(style.top);
 		var newtop = oldtop + 5;
 		cube.style.top = newtop + "px";
 	}
