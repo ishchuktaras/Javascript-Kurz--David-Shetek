77bd418f17a0fa344426a2e31927d575eec096ad
Taras Ishchuk <ishchuktaras@gmail.com>
2022-01-30 18:58:05 +0100

    Posouvaní kosky všemi směry

--- 44.Vypisujeme vlastnosti CSS v JS pomocí getComputedStyle/script.js
+++ 44.Vypisujeme vlastnosti CSS v JS pomocí getComputedStyle/script.js
@@ -1,4 +1,23 @@
 var cube = document.querySelector("#cube");
 
 var style = getComputedStyle(cube);
-console.log(style.left);
+
+document.body.addEventListener("keydown", function(event){
+	if(event.key === "ArrowRight"){
+		var oldleft = parseInt(style.left);
+		var newleft = oldleft + 5;
+		cube.style.left = newleft + "px";
+	} else if(event.key === "ArrowLeft"){
+		var oldleft = parseInt(style.left);
+		var newleft = oldleft - 5;
+		cube.style.left = newleft + "px";
+	} else if(event.key === "ArrowUp"){
+		var oldtop = parseInt(style.top);
+		var newtop = oldtop - 5;
+		cube.style.top = newtop + "px";
+	} else if(event.key === "ArrowDown"){
+		var oldtop = parseInt(style.top);
+		var newtop = oldtop + 5;
+		cube.style.top = newtop + "px";
+	}
+});
