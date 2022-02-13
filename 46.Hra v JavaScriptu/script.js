3f649e4725fe8705ac4a58ce5dc456f16766071e
Taras Ishchuk <ishchuktaras@gmail.com>
2022-02-13 13:10:41 +0100

    Vynulovaní Score

---
+++ 46.Hra v JavaScriptu/script.js
@@ -0,0 +1,4 @@
+document.getElementById('totalScorePlayer-0').textContent = 0;
+document.getElementById('totalScorePlayer-1').textContent = 0;
+document.getElementById('currentScore-0').textContent = 0;
+document.getElementById('currentScore-1').textContent = 0;
