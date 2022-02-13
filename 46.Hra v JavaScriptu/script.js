3255245daa2fb10d8a5e1e6ecc1ed9aeec68f5e1
Taras Ishchuk <ishchuktaras@gmail.com>
2022-02-13 13:18:26 +0100

    Odstránění kostky

--- 46.Hra v JavaScriptu/script.js
+++ 46.Hra v JavaScriptu/script.js
@@ -1,4 +1,8 @@
+// Vynulování a odstraněni kostky
+
 document.getElementById('totalScorePlayer-0').textContent = 0;
 document.getElementById('totalScorePlayer-1').textContent = 0;
 document.getElementById('currentScore-0').textContent = 0;
 document.getElementById('currentScore-1').textContent = 0;
+
+document.querySelector('.diceImage').style.display = 'none';
