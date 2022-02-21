f4f6a62ca3a580323af99510a9436c16dcc49ec4
Taras Ishchuk <ishchuktaras@gmail.com>
2022-02-21 20:23:24 +0100

    Prototype

--- 48.JavaScript (OOP): konstruktor a funkce, return/script.js
+++ 48.JavaScript (OOP): konstruktor a funkce, return/script.js
@@ -9,22 +9,18 @@ var Person = function(name,yearBirth,job){
   this.name = name;
   this.yearBirth = yearBirth;
   this.job = job;
-  // Metoda
-  this.calcAge = function(){
-    return ( 2022 - this.yearBirth);
-  }
+  // ***** Metoda *****
+  // this.calcAge = function(){
+  //   return ( 2022 - this.yearBirth);
+  // } 
+}
+
+Person.prototype.calcAge = function(){
+  return ( 2022 - this.yearBirth);
 }
 
 var taras = new Person ('Taras', '1981', 'programátor');
 var marta = new Person ('Marta', '1991', 'prodavačka');
 
-
-console.log(taras.name);
-console.log(taras.yearBirth);
-console.log(taras.job);
 console.log(taras.calcAge());
-
-console.log(marta.name);
-console.log(marta.yearBirth);
-console.log(marta.job);
-console.log (marta.calcAge());
+console.log(marta.calcAge());
